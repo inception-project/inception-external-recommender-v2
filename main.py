63750f21f4eaf27f1ef3d218904c4ec0e0556d2b
Jan-Christoph Klie <git@mrklie.com>
2021-06-05 17:38:52 +0200

    Main: Add spacy ner

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from galahad.server import GalahadServer
+from galahad.server.contrib.ner.spacy_ner import SpacyNerClassifier
 from galahad.server.contrib.sentence_classification.sklearn_sentence_classifier import \
     SklearnSentenceClassifier
 
 app = GalahadServer()
 app.add_classifier("sklearn1", SklearnSentenceClassifier())
 app.add_classifier("sklearn2", SklearnSentenceClassifier())
+app.add_classifier("spacy_ner", SpacyNerClassifier("en_core_web_sm")
+)
