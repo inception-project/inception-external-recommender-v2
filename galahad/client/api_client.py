01d1e3166f300c084a54abefd8da66394e07728d
Falko1 <falko.helm@gmx.de>
2021-12-16 19:24:56 +0100

    #2-add-api-client
    -listing, creation and deletion of datasets works

--- galahad/client/api_client.py
+++ galahad/client/api_client.py
@@ -1,5 +1,25 @@
-import typing
+from typing import List, Dict, Optional
 import requests
+import logging
+
+
+class ApiClientError(Exception):
+    pass
+
+
+# Wrong status code is returned
+class StatusCodeError(ApiClientError):
+    pass
+
+
+# Response is not correct
+class ResponseError(ApiClientError):
+    pass
+
+
+# Data does not exist but should
+class DataNonExistentError(ApiClientError):
+    pass
 
 
 class GalahadClient:
@@ -7,16 +27,67 @@ class GalahadClient:
     def __init__(self, endpoint_url: str):
         self.endpoint_url = endpoint_url.removesuffix("/")
 
-    def is_connected(self) -> [bool, str]:
+    def is_connected(self) -> bool:
         response = requests.get(self.endpoint_url + "/ping")
         if response.status_code != 200:
-            return [False, "Wrong status code"]
+            logging.exception("StatusCodeError")
+            return False
         if response.json() != {"ping": "pong"}:
-            return [False, "Wrong response text"]
-        return [True, "OK"]
+            logging.exception("ResponseError")
+            return False
+        return True
 
-    # def get_recommenders(self):
-    #    return 0
+    def list_datasets(self) -> List[str]:
+        response = requests.get(self.endpoint_url + "/dataset")
+        if response.status_code != 200:
+            raise StatusCodeError("Status Code should be 200 but is " + str(response.status_code))
+        return response.json()["names"]
 
-    # def create_dataset(self, name: str):
-    #    print(name)
+    def contains_dataset(self, dataset_id: str) -> bool:
+        return dataset_id in self.list_datasets()
+
+    def create_dataset(self, dataset_id: str, data=None):
+        if data is None:
+            data = {}
+        response = requests.put(self.endpoint_url + "/dataset/" + dataset_id, data)
+        if response.status_code == 409:
+            logging.exception("Dataset with id: " + dataset_id + " already exists")
+            return None
+        if response.status_code != 204:
+            raise StatusCodeError("Problem with dataset_id: " + dataset_id +
+                                  ". Status Code should be 204 but is " + str(response.status_code))
+        if response.text != "":
+            raise ResponseError("Problem with dataset_id: " + dataset_id + ". JSON response was not empty")
+        return None
+
+    def delete_dataset(self, dataset_id: str):
+        response = requests.delete(self.endpoint_url + "/dataset/" + dataset_id)
+        if response.status_code == 404:
+            logging.exception("Dataset with id: " + dataset_id + " does not exist")
+            return None
+        if response.status_code != 204:
+            raise StatusCodeError("Problem with dataset_id: " + dataset_id +
+                                  ". Status Code should be 204 but is " + str(response.status_code))
+        if response.text != "":
+            raise ResponseError("Problem with dataset_id: " + dataset_id + ". JSON response was not empty")
+        return None
+
+    def delete_datasets(self, dataset_ids: List[str]):
+        for dataset_id in dataset_ids:
+            self.delete_dataset(dataset_id)
+
+    def delete_all_datasets(self):
+        self.delete_datasets(self.list_datasets())
+
+    def list_documents_in_dataset(self, dataset_id) -> Dict[str, int]:
+        response = requests.get(self.endpoint_url + "/dataset/" + dataset_id)
+        if response.status_code == 404:
+            raise DataNonExistentError("Dataset with id: " + dataset_id + " does not exist")
+        if response.status_code != 200:
+            raise StatusCodeError("Problem with dataset_id: " + dataset_id +
+                                  ". Status Code should be 200 but is " + str(response.status_code))
+
+        return dict(zip(response.json()["names"], response.json()["versions"]))
+
+    #TODO def create_document_in_dataset(self):
+    #TODO /dataset/ besser loesen
--- tests/client/test_api_client.py
+++ tests/client/test_api_client.py
@@ -1,42 +1,82 @@
-from tempfile import TemporaryDirectory
-
 from galahad.client.api_client import GalahadClient
-import pytest
-from pathlib import Path
-import uvicorn
-
 from galahad.server import GalahadServer
+from galahad.server.dataclasses import Document, DocumentList
+import logging
+import pytest
+
+server = GalahadServer()
+host = "127.0.0.1"
+port = 8000
+global address
+address = "http://" + host + ":" + str(port)
+client = GalahadClient(address)
 
 
-@pytest.fixture
-def server():
-    tmp = TemporaryDirectory()
-
-    global tmpdir
-    tmpdir = Path(tmp.name)
-
-    host = "127.0.0.1"
-    port = 8000
-
-    global address
-    address = "https://" + host + "/" + str(port)
-
-    server = GalahadServer(data_dir=tmpdir)
-    uvicorn.run(server, host=host, port=port)
-
-    yield server
-    tmp.cleanup()
-
-
-@pytest.fixture
-def client(server):
-    # peter = server.state.data_dir
-    yield GalahadClient(address)
-
-
-def test_creation(client: GalahadClient):
+def test_creation():
     assert client.endpoint_url == address
 
 
-def test_is_connected(client: GalahadClient):
-    assert client.is_connected()[0]
+def test_is_connected():
+    assert client.is_connected()
+
+
+def test_create_dataset(caplog):
+    client.delete_all_datasets()
+
+    client.create_dataset("dataset1", {"docA": "lorem ipsum"})
+    assert len(caplog.messages) == 0
+
+    client.create_dataset("dataset2", {"docB": "foo bar"})
+    assert len(caplog.messages) == 0
+
+    client.create_dataset("dataset2", {"docB": "foo bar"})
+    assert len(caplog.messages) == 1 and caplog.messages[0] == "Dataset with id: dataset2 already exists"
+
+
+def test_list_datasets():
+    client.delete_all_datasets()
+
+    client.create_dataset("dataset1", {"docA": "lorem ipsum"})
+    client.create_dataset("dataset2", {"docB": "foo bar"})
+
+    assert ["dataset1", "dataset2"] == client.list_datasets()
+    assert client.contains_dataset("dataset1")
+    assert client.contains_dataset("dataset2")
+
+
+def test_delete_dataset(caplog):
+    client.delete_all_datasets()
+
+    client.create_dataset("dataset1", {"docA": "lorem ipsum"})
+
+    client.delete_dataset("dataset1")
+    assert len(caplog.messages) == 0
+
+    client.delete_dataset("dataset1")
+    assert len(caplog.messages) == 1 and caplog.messages[0] == "Dataset with id: dataset1 does not exist"
+
+
+def test_list_documents_in_dataset():
+    client.delete_all_datasets()
+
+    client.create_dataset("test_dataset")
+    expected_names = []
+    expected_versions = []
+
+    for i in range(3):
+        name = f"test_document{i}"
+        version = i
+
+        expected_names.append(name)
+        expected_versions.append(i)
+
+        request = Document.Config.schema_extra["example"]
+        request["version"] = version
+
+        response = client.create_document(f"test_dataset/{name}")
+
+    response = client.list_documents_in_dataset("test_dataset")
+    print("asdf")
+
+
+    assert client.list_documents_in_dataset("dataset1") == input_docs
