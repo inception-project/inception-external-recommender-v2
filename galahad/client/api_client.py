53630536e2befdb1da7996a8e624bdf87b8ad021
Falko1 <falko.helm@gmx.de>
2021-12-17 18:41:49 +0100

    #2-add-api-client
    -manipulating datasets and documents works

--- galahad/client/api_client.py
+++ galahad/client/api_client.py
@@ -1,6 +1,9 @@
-from typing import List, Dict, Optional
+from typing import List, Dict
+
 import requests
+from requests import Response
 import logging
+from galahad.server.dataclasses import Document
 
 
 class ApiClientError(Exception):
@@ -22,6 +25,51 @@ class DataNonExistentError(ApiClientError):
     pass
 
 
+# Invalid name for file or folder
+class NamingError(ApiClientError):
+    pass
+
+
+def create_error_message(variables: Dict[str, str]) -> str:
+    if len(variables.keys()) == 0:
+        error_message = ""
+    else:
+        error_message = "The problem appeared with the variables "
+        for variable_name in variables.keys():
+            error_message = error_message + variable_name + ": " + variables[variable_name] + " and "
+        error_message = error_message.removesuffix(" and ")
+        error_message = error_message + "."
+    return error_message
+
+
+def is_status_ok(expected_status: int, given_status: int, variables: Dict[str, str]):
+    if expected_status != given_status:
+        error_message = create_error_message(variables)
+        raise StatusCodeError(
+            "Expected HTTP status is " + str(expected_status) + ", but " +
+            str(given_status) + " was received from server. " + error_message)
+
+
+def is_response_ok(expected_response: str, given_response: str, variables: Dict[str, str]):
+    if expected_response != given_response:
+        error_message = create_error_message(variables)
+        raise ResponseError("Expected JSON response is \"" + expected_response +
+                            "\", but \"" + given_response + "\" was received from server. " + error_message)
+
+
+def is_response_empty(response: Response, variables: Dict[str, str]):
+    is_status_ok(204, response.status_code, variables)
+    is_response_ok("", response.text, variables)
+
+
+# TODO: would it be better for the performance if we checked the naming before sending the request to the server?
+def is_naming_ok(given_status: int, variables: Dict[str, str]):
+    if given_status == 422:
+        error_message = create_error_message(variables)
+        raise NamingError("Naming for one of the variables is invalid. "
+                          "Please look at the documentation for correct naming. " + error_message)
+
+
 class GalahadClient:
 
     def __init__(self, endpoint_url: str):
@@ -37,40 +85,30 @@ class GalahadClient:
             return False
         return True
 
+    # output is sorted by dataset name
     def list_datasets(self) -> List[str]:
         response = requests.get(self.endpoint_url + "/dataset")
-        if response.status_code != 200:
-            raise StatusCodeError("Status Code should be 200 but is " + str(response.status_code))
+        is_status_ok(200, response.status_code, {})
         return response.json()["names"]
 
     def contains_dataset(self, dataset_id: str) -> bool:
         return dataset_id in self.list_datasets()
 
-    def create_dataset(self, dataset_id: str, data=None):
-        if data is None:
-            data = {}
-        response = requests.put(self.endpoint_url + "/dataset/" + dataset_id, data)
+    def create_dataset(self, dataset_id: str):
+        response = requests.put(self.endpoint_url + "/dataset/" + dataset_id, {})
+        is_naming_ok(response.status_code, {"dataset_id": dataset_id})
         if response.status_code == 409:
             logging.exception("Dataset with id: " + dataset_id + " already exists")
             return None
-        if response.status_code != 204:
-            raise StatusCodeError("Problem with dataset_id: " + dataset_id +
-                                  ". Status Code should be 204 but is " + str(response.status_code))
-        if response.text != "":
-            raise ResponseError("Problem with dataset_id: " + dataset_id + ". JSON response was not empty")
-        return None
+        is_response_empty(response, {"dataset_id": dataset_id})
 
     def delete_dataset(self, dataset_id: str):
         response = requests.delete(self.endpoint_url + "/dataset/" + dataset_id)
+        is_naming_ok(response.status_code, {"dataset_id": dataset_id})
         if response.status_code == 404:
             logging.exception("Dataset with id: " + dataset_id + " does not exist")
             return None
-        if response.status_code != 204:
-            raise StatusCodeError("Problem with dataset_id: " + dataset_id +
-                                  ". Status Code should be 204 but is " + str(response.status_code))
-        if response.text != "":
-            raise ResponseError("Problem with dataset_id: " + dataset_id + ". JSON response was not empty")
-        return None
+        is_response_empty(response, {"dataset_id": dataset_id})
 
     def delete_datasets(self, dataset_ids: List[str]):
         for dataset_id in dataset_ids:
@@ -79,15 +117,46 @@ class GalahadClient:
     def delete_all_datasets(self):
         self.delete_datasets(self.list_datasets())
 
+    # The new document of the same name will override an existing one!
+    def create_document_in_dataset(self, dataset_id: str, document_id: str, document: Document,
+                                   auto_create_dataset=False):
+        response = requests.put(self.endpoint_url + "/dataset/" + dataset_id + "/" + document_id, json=document)
+        is_naming_ok(response.status_code, {"dataset_id": dataset_id, "document_id": document_id})
+        if response.status_code == 404:
+            if auto_create_dataset:
+                self.create_dataset(dataset_id)
+                response = requests.put(self.endpoint_url + "/dataset/" + dataset_id + "/" + document_id, json=document)
+            else:
+                raise DataNonExistentError("The dataset for the given id: " + dataset_id +
+                                           " does not exist. To create it, "
+                                           "set the optional parameter auto_create_dataset to True")
+
+        is_response_empty(response, {"dataset_id": dataset_id, "document_id": document_id})
+
+    # result is sorted by doc id
     def list_documents_in_dataset(self, dataset_id) -> Dict[str, int]:
         response = requests.get(self.endpoint_url + "/dataset/" + dataset_id)
+        is_naming_ok(response.status_code, {"dataset_id": dataset_id})
         if response.status_code == 404:
             raise DataNonExistentError("Dataset with id: " + dataset_id + " does not exist")
-        if response.status_code != 200:
-            raise StatusCodeError("Problem with dataset_id: " + dataset_id +
-                                  ". Status Code should be 200 but is " + str(response.status_code))
+        is_status_ok(200, response.status_code, {"dataset_id": dataset_id})
 
         return dict(zip(response.json()["names"], response.json()["versions"]))
 
-    #TODO def create_document_in_dataset(self):
-    #TODO /dataset/ besser loesen
+    def dataset_contains_document(self, dataset_id: str, document_id: str) -> bool:
+        return document_id in list(self.list_documents_in_dataset(dataset_id).keys())
+
+    def delete_document_in_dataset(self, dataset_id: str, document_id: str):
+        response = requests.delete(self.endpoint_url + "/dataset/" + dataset_id + "/" + document_id)
+        is_naming_ok(response.status_code, {"dataset_id": dataset_id, "document_id": document_id})
+        if response.status_code == 404:
+            raise DataNonExistentError("Dataset with id: " + dataset_id + " does not exist")
+        is_response_empty(response, {"dataset_id": dataset_id, "document_id": document_id})
+
+    def delete_all_documents_in_dataset(self, dataset_id: str):
+        for document_id in list(self.list_documents_in_dataset(dataset_id).keys()):
+            self.delete_document_in_dataset(dataset_id, document_id)
+
+    def delete_all_documents(self):
+        for dataset_id in self.list_datasets():
+            self.delete_all_documents_in_dataset(dataset_id)
--- tests/client/test_api_client.py
+++ tests/client/test_api_client.py
@@ -1,7 +1,6 @@
-from galahad.client.api_client import GalahadClient
+from galahad.client.api_client import GalahadClient, NamingError, DataNonExistentError
 from galahad.server import GalahadServer
-from galahad.server.dataclasses import Document, DocumentList
-import logging
+from galahad.server.dataclasses import Document
 import pytest
 
 server = GalahadServer()
@@ -23,22 +22,27 @@ def test_is_connected():
 def test_create_dataset(caplog):
     client.delete_all_datasets()
 
-    client.create_dataset("dataset1", {"docA": "lorem ipsum"})
+    client.create_dataset("dataset1")
+    assert len(caplog.messages) == 0
+    assert len(client.list_datasets()) == 1
+
+    client.create_dataset("dataset2")
     assert len(caplog.messages) == 0
 
-    client.create_dataset("dataset2", {"docB": "foo bar"})
-    assert len(caplog.messages) == 0
-
-    client.create_dataset("dataset2", {"docB": "foo bar"})
+    client.create_dataset("dataset2")
     assert len(caplog.messages) == 1 and caplog.messages[0] == "Dataset with id: dataset2 already exists"
+
+    with pytest.raises(NamingError):
+        client.create_dataset("-")
 
 
 def test_list_datasets():
     client.delete_all_datasets()
 
-    client.create_dataset("dataset1", {"docA": "lorem ipsum"})
-    client.create_dataset("dataset2", {"docB": "foo bar"})
+    client.create_dataset("dataset2")
+    client.create_dataset("dataset1")
 
+    # sorted by name
     assert ["dataset1", "dataset2"] == client.list_datasets()
     assert client.contains_dataset("dataset1")
     assert client.contains_dataset("dataset2")
@@ -47,36 +51,84 @@ def test_list_datasets():
 def test_delete_dataset(caplog):
     client.delete_all_datasets()
 
-    client.create_dataset("dataset1", {"docA": "lorem ipsum"})
+    client.create_dataset("dataset1")
 
     client.delete_dataset("dataset1")
     assert len(caplog.messages) == 0
+    assert len(client.list_datasets()) == 0
 
     client.delete_dataset("dataset1")
     assert len(caplog.messages) == 1 and caplog.messages[0] == "Dataset with id: dataset1 does not exist"
 
+    with pytest.raises(NamingError):
+        client.delete_dataset("-")
+
+
+def test_create_document_in_dataset():
+    doc = Document.Config.schema_extra["example"]
+    client.delete_all_datasets()
+
+    with pytest.raises(DataNonExistentError):
+        client.create_document_in_dataset("dataset1", "doc1", doc)
+
+    client.create_document_in_dataset("dataset1", "doc1", doc, True)
+
+    client.create_dataset("dataset2")
+    client.create_document_in_dataset("dataset2", "doc1", doc)
+
+    with pytest.raises(NamingError):
+        client.create_document_in_dataset("-", "doc1", doc)
+
+    with pytest.raises(NamingError):
+        client.create_document_in_dataset("dataset1", "-", doc)
+
 
 def test_list_documents_in_dataset():
     client.delete_all_datasets()
+    doc = Document.Config.schema_extra["example"]
 
-    client.create_dataset("test_dataset")
-    expected_names = []
-    expected_versions = []
+    client.create_dataset("dataset1")
 
-    for i in range(3):
-        name = f"test_document{i}"
-        version = i
+    doc["version"] = 7
+    client.create_document_in_dataset("dataset1", "doc3", doc)
+    doc["version"] = 2
+    client.create_document_in_dataset("dataset1", "doc1", doc)
+    doc["version"] = 8
+    client.create_document_in_dataset("dataset1", "doc2", doc)
 
-        expected_names.append(name)
-        expected_versions.append(i)
+    response = client.list_documents_in_dataset("dataset1")
 
-        request = Document.Config.schema_extra["example"]
-        request["version"] = version
+    # sorted by doc id
+    assert list(response.keys()) == ["doc1", "doc2", "doc3"]
+    assert list(response.values()) == [2, 8, 7]
 
-        response = client.create_document(f"test_dataset/{name}")
+    with pytest.raises(DataNonExistentError):
+        client.list_documents_in_dataset("dataset2")
 
-    response = client.list_documents_in_dataset("test_dataset")
-    print("asdf")
+    with pytest.raises(NamingError):
+        client.list_documents_in_dataset("-")
 
 
-    assert client.list_documents_in_dataset("dataset1") == input_docs
+def test_delete_document_in_dataset():
+    client.delete_all_datasets()
+    doc = Document.Config.schema_extra["example"]
+
+    client.create_dataset("dataset1")
+
+    client.create_document_in_dataset("dataset1", "doc1", doc)
+
+    assert len(client.list_documents_in_dataset("dataset1")) == 1
+
+    client.delete_document_in_dataset("dataset1", "doc1")
+    assert len(client.list_documents_in_dataset("dataset1")) == 0
+
+    client.delete_document_in_dataset("dataset1", "doc2")
+
+    with pytest.raises(DataNonExistentError):
+        client.delete_document_in_dataset("dataset2", "doc1")
+
+    with pytest.raises(NamingError):
+        client.delete_document_in_dataset("-", "doc1")
+
+    with pytest.raises(NamingError):
+        client.delete_document_in_dataset("dataset1", "-")
