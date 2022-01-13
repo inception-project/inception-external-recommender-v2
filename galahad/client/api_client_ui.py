fce7b335fa7bc728470aabfa6240f3cb77e75d2c
Falko1 <falko.helm@gmx.de>
2022-01-13 13:44:52 +0100

    #10-gradio-ui-for-api
    -SpacyNER works

--- galahad/client/api_client_ui.py
+++ galahad/client/api_client_ui.py
@@ -5,29 +5,29 @@ from galahad.server import GalahadServer
 from galahad.server.contrib.ner.spacy_ner import SpacyNerClassifier
 
 models = ["SpacyNER"]
-
-server = GalahadServer()
-spacy_ner = SpacyNerClassifier("en_core_web_sm")
-server.add_classifier("SpacyNER", spacy_ner)
 client = api_client.GalahadClient("http://127.0.0.1:8000")
 
 
 def predict(model, sentence):
+
     tokens = sentence.split(" ")
     doc = formats.build_span_classification_request([tokens])
     print(client.list_all_classifiers())
-    #annotated_doc = client.predict_on_document("SpacyNER", "en_core_web_sm", doc)
+    annotated_doc = client.predict_on_document("SpacyNER", "en_core_web_sm", doc)
+    ret = ""
+    for annotation in annotated_doc["annotations"]["t.annotation"]:
+        ret = ret + annotated_doc["text"][annotation["begin"]:annotation["end"]] + ": " + annotation["features"]["f.value"] + "; "
 
-    return "Hello " + model + "!!" + sentence
+    return ret.removesuffix("; ")
 
 
 iface = gr.Interface(fn=predict,
                      inputs=[gr.inputs.Dropdown(models, type="value", default=None, label="Choose a model"),
                              gr.inputs.Textbox(placeholder="Type sentence here...", label="Input Sentence")],
                      outputs="text",
-                     examples=[["SpacyNER", "Neil Armstrong was a NASA astronaut."],
-                               ["SpacyNER", "The jazz musician Louis Armstrong was born in New Orleans."],
-                               ["SpacyNER", "Lance Armstrong won the Tour de France several times."]],
+                     examples=[["SpacyNER", "Neil Armstrong was a NASA astronaut"],
+                               ["SpacyNER", "The jazz musician Louis Armstrong was born in New Orleans"],
+                               ["SpacyNER", "Lance Armstrong won the Tour de France several times"]],
                      title="Named Entity Recognition",
                      description="Named Entity Recognition (NER) is the task of identifying persons, places, "
                                  "institutions etc. in a given sentence.")
